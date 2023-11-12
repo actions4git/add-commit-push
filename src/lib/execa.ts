adac2303f9810b60b70eda25801f88892be81691
Jacob Hummer <jcbhmr@outlook.com>
2023-11-12 12:57:07 -0600

    try this

--- src/lib/execa.ts
+++ src/lib/execa.ts
@@ -14,14 +14,15 @@ export function $(strings, ...values) {
   let argv = cmdWithKeys.split(/\s+/);
   argv = argv.map((arg) => arg.replace(keyRe, (m, i) => values[i]));
   const argv0 = argv.shift();
-  const cp = spawn(argv0, argv, this);
+  const cp = spawn(argv0, argv, this ?? {});
   const stdoutP = cp.stdout && new Response(Readable.toWeb(cp.stdout)).text();
   const stderrP = cp.stderr && new Response(Readable.toWeb(cp.stderr)).text();
-  cp.then = once(cp, "exit").then(async ([exitCode]) => {
+  const p = once(cp, "exit").then(async ([exitCode, signal]) => {
     const res = {
       stdout: await stdoutP,
       stderr: await stderrP,
-      exitCode: exitCode,
+      exitCode,
+      signal,
     };
     if ((this?.reject ?? true) && exitCode) {
       throw res;
@@ -29,5 +30,6 @@ export function $(strings, ...values) {
       return res;
     }
   });
+  cp.then = p.then.bind(p)
   return cp;
 }
