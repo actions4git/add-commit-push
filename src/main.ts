1d7e5b19a2f274ac1844c61e6e14582dda1a9b0d
Jacob Hummer <jcbhmr@outlook.com>
2023-11-14 20:56:19 +0000

    much more basic logic

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,19 @@ import { $ } from "execa";
 const githubContext = {
   actor: process.env.GITHUB_ACTOR!,
   actor_id: process.env.GITHUB_ACTOR_ID!,
-}
+};
 
 function getNameEmailInput(
   input: string,
-  options: { required: true },
+  options: { required: true }
 ): [string, string];
 function getNameEmailInput(
   input: string,
-  options?: { required?: boolean },
+  options?: { required?: boolean }
 ): [string | null, string | null];
 function getNameEmailInput(
   input: string,
-  options: { required?: boolean } = {},
+  options: { required?: boolean } = {}
 ) {
   const { required = false } = options;
   const githubActionsRe = /^\s*@?github[-_]?actions(?:\[bot\])?\s*$/;
@@ -48,64 +48,46 @@ function getNameEmailInput(
 }
 
 const rootPath = resolve(core.getInput("path"));
-const addPathspec = core.getInput("add-pathspec")
-  ? core.getMultilineInput("add-pathspec")
-  : null;
-const addForce = core.getBooleanInput("add-force");
-const [GIT_AUTHOR_NAME, GIT_AUTHOR_EMAIL] = getNameEmailInput("commit-author", {
-  required: true,
-});
-const [
-  GIT_COMMITTER_NAME = GIT_AUTHOR_NAME,
-  GIT_COMMITTER_EMAIL = GIT_AUTHOR_EMAIL,
-] = getNameEmailInput("commit-committer");
-const commitMessage = core.getInput("commit-message");
-const pushRepository = core.getInput("push-repository");
 
-if (addPathspec) {
-  await $({
-    stdio: "inherit",
-    cwd: rootPath,
-  })`git add ${addForce ? "--force" : []} -- ${addPathspec}`;
-} else {
-  await $({
-    stdio: "inherit",
-    cwd: rootPath,
-  })`git add ${addForce ? "--force" : []} --all`;
+add: {
+  const addPathspec = core.getInput("add-pathspec")
+    ? core.getMultilineInput("add-pathspec")
+    : null;
+  const addForce = core.getBooleanInput("add-force");
+
+  if (addPathspec) {
+    await $({
+      stdio: "inherit",
+      cwd: rootPath,
+    })`git add ${addForce ? "--force" : []} -- ${addPathspec}`;
+  } else {
+    await $({
+      stdio: "inherit",
+      cwd: rootPath,
+    })`git add ${addForce ? "--force" : []} --all`;
+  }
 }
 
-const { exitCode } = await $({
-  cwd: rootPath,
-  reject: false,
-})`git diff --cached`;
-if (exitCode) {
-  core.info(`No changes to commit`);
-} else {
-  let pushRefspec = core.getInput("push-refspec");
-  let pushForce = core.getInput("push-force")
-    ? core.getBooleanInput("push-force")
-    : null;
-  if (!pushRefspec) {
-    const { exitCode, stdout } = await $({
-      cwd: rootPath,
-      reject: false,
-    })`git symbolic-ref HEAD`;
-    if (exitCode) {
-      const { stdout } = await $({ cwd: rootPath })`git tag --points-at HEAD`;
-      console.assert(stdout, "no stdout from listing tags");
-      const tags = stdout.split(/\r?\n/g);
-      console.assert(tags.length >= 1, `tags=${tags} longer than 1`);
-      pushRefspec = tags[0];
-    } else {
-      pushRefspec = stdout;
-    }
-  }
-  if (pushForce == null) {
-    const { exitCode } = await $({
-      cwd: rootPath,
-      reject: false,
-    })`git symbolic-ref HEAD`;
-    pushForce = !!exitCode;
+let committed: boolean;
+commit: {
+  const [GIT_AUTHOR_NAME, GIT_AUTHOR_EMAIL] = getNameEmailInput(
+    "commit-author",
+    { required: true }
+  );
+  let [GIT_COMMITTER_NAME, GIT_COMMITTER_EMAIL] =
+    getNameEmailInput("commit-committer");
+  GIT_COMMITTER_NAME ??= GIT_AUTHOR_NAME;
+  GIT_COMMITTER_EMAIL ??= GIT_AUTHOR_EMAIL;
+  const commitMessage = core.getInput("commit-message");
+
+  const { exitCode } = await $({
+    cwd: rootPath,
+    reject: false,
+  })`git diff --cached`;
+  if (exitCode) {
+    committed = false;
+    core.setOutput("committed", false);
+    break commit;
   }
 
   await $({
@@ -118,14 +100,35 @@ if (exitCode) {
       GIT_COMMITTER_EMAIL,
     },
   })`git commit --message ${commitMessage}`;
+
+  core.setOutput("committed", true);
+  committed = true;
+
   const { stdout } = await $({
     cwd: rootPath,
   })`git rev-parse HEAD`;
+  core.setOutput("commit-sha", stdout);
+}
+
+push: {
+  const pushRepository = core.getInput("push-repository");
+  let pushRefspec = core.getInput("push-refspec") || null;
+  let pushForce = core.getBooleanInput("push-force");
+
+  const { stdout } = await $({
+    cwd: rootPath,
+  })`git cherry -v`;
+  if (!stdout) {
+    core.setOutput("pushed", false);
+    break push;
+  }
 
   await $({
     stdio: "inherit",
     cwd: rootPath,
-  })`git push ${
-    pushForce ? "--force" : []
-  } ${pushRepository} ${stdout}:${pushRefspec}`;
+  })`git push ${pushForce ? "--force" : []} ${pushRepository} ${
+    pushRefspec ? pushRefspec : []
+  }`;
+
+  core.setOutput("pushed", true);
 }
